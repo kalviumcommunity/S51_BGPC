58646389e5b48c1d52c645e9c36470dc5e49c578
Moneshbb <2005.monesh@gmail.com>
2024-02-27 10:42:52 +0530

    entity component

---
+++ frontEnd/GPC/src/Component/Entity.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import data from  '../data.json';
+import '../Styles/entity.css'
+
+const Entity = () => {
+  
+  return (
+    <div className='entity'>
+      <h2>Entity</h2>
+      <p>PC: {data.PC}</p>
+      <p>CPU: {data.CPU}</p>
+      <p>GPU: {data.GPU}</p>
+      <p>RAM: {data.RAM}</p>
+      <p>Storage: {data.Storage}</p>
+      <p>Cabinet: {data.Cabinet}</p>
+      <p>Price: {data.Price_INR}</p>
+    </div>
+  )
+}
+
+export default Entity
